488447f95e48b89d1e5ec5fce119c5884fecea15
Ryan Park <rjpark2004@gmail.com>
2019-09-19 16:00:30 -0700

    got rid of some more unecessary files

--- scouting-server/src/app.js
+++ scouting-server/src/app.js
@@ -5,23 +5,13 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
-
-app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req,res) => {
-    res.render('index', {
-        title: 'Weather app',
-        name: 'Ryan Park'
-    })
+    res.render('index')
 })
 
-app.get('/weather', (req, res) => {c
-    res.send({
-        forecast: 'It is snowing',
-        location: 'Philadelphia'
-    })
-})
+
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000')
